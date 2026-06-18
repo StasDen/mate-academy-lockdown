eb2f79c0dea265f119f37c2d16bf08862f5211da
stasden <lohopusik@gmail.com>
2026-06-18 12:57:44 +0300

    Remove comments

--- app/cafe.py
+++ app/cafe.py
@@ -1,10 +1,3 @@
-# FIXME
-# Please do not import multiple modules like this. To improve readability, use parentheses instead:
-# from app.errors import (
-#     NotVaccinatedError,
-#     OutdatedVaccineError,
-#     NotWearingMaskError
-# )
 from app.errors import NotVaccinatedError, \
                         OutdatedVaccineError, \
                         NotWearingMaskError
@@ -16,11 +9,11 @@ class Cafe:
         self.name = name
 
     def visit_cafe(self, vstr: dict) -> str:
-        if "vaccine" not in vstr.keys() or vstr["vaccine"] == None:  # FIXME: 2nd statement could be more efficient: '... or not vstr["vaccine"]'
+        if "vaccine" not in vstr.keys() or vstr["vaccine"] == None:
             raise NotVaccinatedError("not vaccinated")
         elif vstr["vaccine"]["expiration_date"] < datetime.date.today():
-            raise OutdatedVaccineError("outdated")  # FIXME: error messages should be more descriptive (e.g., 'Client's vaccine is outdated')
+            raise OutdatedVaccineError("outdated")
         elif not vstr["wearing_a_mask"]:
             raise NotWearingMaskError("not wearing mask")
         else:
-            return "Welcome to " + self.name  # FIXME: avoid using unnecessary 'else' statement
+            return "Welcome to " + self.name
--- app/main.py
+++ app/main.py
@@ -3,19 +3,18 @@ from app.errors import (NotWearingMaskError,
                         NotVaccinatedError,
                         OutdatedVaccineError)
 
-# FIXME: use annotations for function parameters (class 'Cafe' is already imported)
 def go_to_cafe(friends, cafe) -> str:
-    total = 0  # NOTE: the variable names should be aligned with implemented flow. I suggest renaming it to 'masks_to_buy'
+    total = 0
 
     for i in range(len(friends)):
         try:
             cafe.visit_cafe(friends[i])
         except (NotVaccinatedError, OutdatedVaccineError):
-            return 'All friends should be vaccinated'  # FIXME: use one style of quotes in your code (double quotes are preferable)
+            return 'All friends should be vaccinated'
         except NotWearingMaskError:
             total += 1
 
     if total:
         return f'Friends should buy {total} masks'
     else:
-        return f'Friends can go to {cafe.name}'  # FIXME: avoid using unnecessary 'else' statement
+        return f'Friends can go to {cafe.name}'
